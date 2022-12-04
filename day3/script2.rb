7793ac40c93fdef5cdbc747528f447e3d2003b08
Aloxion <58940157+Aloxion@users.noreply.github.com>
2022-12-04 16:14:09 +0100

    day3part2

--- day3/script2.rb
+++ day3/script2.rb
@@ -1,38 +1,43 @@
 
-rucksacks = []
-sum = 0
 def find_value(char)
   alphabet = [*'a'..'z', *'A'..'Z']
   alphabet.length.times {|i| return i+1 if alphabet[i] == char }
 end
-
 def find_common_char(map)
   first_hash = {}
   second_hash = {}
-  map.each do | comp |
-    map[index].each_char.with_index do |char, i|
-      first_hash[i+1] = char
-    end
-    map[comp[0]].each_char.with_index do |char, i|
-      second_hash[i+1] = char
-    end
-    #third > first and second
-    p map[comp[2]]
-    sleep(5)
-    map[comp[2]].each_char do |char|
-      if first_hash.key(char) and second_hash.key(char)
-        sum = find_value(char)
+  second_map = []
+  map.each_with_index  do | comp, index |
+    if index == 0
+      comp[0].each_char.with_index do |char, i|
+        first_hash[i+1] = char
+      end
+    elsif index == 1
+      second_map = comp
+      comp[0].each_char.with_index do |char, i|
+        second_hash[i+1] = char
+      end
+    elsif index > 1
+      comp[0].each_char do |char|
+        if first_hash.key(char) and second_hash.key(char)
+          return find_value(char)
+        end
       end
     end
-
-
   end
-
+  return 0
 end
 
+rucksacks = []
+sum = 0
 File.open('input.txt').each_line {|line| rucksacks.append(compartments = [].append(line).map(&:chomp))}
 grouped_rucksacks = []
-rucksacks.each_index {|index| grouped_rucksacks.append([rucksacks[index], rucksacks[index+1], rucksacks[index+2],])}
-grouped_rucksacks.each {|map| find_common_char(map)}
-
+i = 0
+rucksacks.each do
+  if i < rucksacks.length
+    grouped_rucksacks.append([rucksacks[i], rucksacks[i+1], rucksacks[i+2],])
+  end
+  i+=3
+end
+grouped_rucksacks.each {|map| sum += find_common_char(map)}
 p sum
